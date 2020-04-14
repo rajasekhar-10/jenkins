736b622b9f9fa72c7e17a20c096e2d594a3b41d2
Ravi <ravi2krishna@gmail.com>
2020-04-15 00:34:42 +0530

    unit testing - unstable build

---
+++ 04-Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+pipeline {
+   agent any
+
+   stages {
+      stage('Fetch Code') {
+         steps {
+            git branch: 'master', url: 'https://github.com/ravi2krishna/JavaCalculator-Jenkins.git'
+         }
+      }
+      stage('Unit Testing') {
+         steps {
+            // `mvn test` returns non-zero on test failures
+            // using `true` to allow the Pipeline to continue nonetheless
+            sh 'mvn test || true'
+            junit 'target/surefire-reports/*.xml'
+         }
+      }
+      stage('Deply') {
+         steps {
+            sleep 3
+            echo "Application Deployed"
+         }
+      }
+   }
+}

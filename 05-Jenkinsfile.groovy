d07d008e643f2b9a20faaa878d8784a1bd6d8eca
Ravi <ravi2krishna@gmail.com>
2020-04-15 00:49:31 +0530

    unit testing - build failure

---
+++ 05-Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+pipeline {
+   agent any
+
+   stages {
+      stage('Fetch Code') {
+         steps {
+            git branch: 'master', url: 'https://github.com/ravi2krishna/JavaCalculator-Jenkins.git'
+         }
+      }
+      stage('Unit Testing') {
+         steps {
+            // `mvn test` fails and returns non-zero on test failures
+            sh 'mvn test'
+            junit '**/target/surefire-reports/*.xml'
+         }
+      }
+      stage('Deply') {
+         steps {
+            sleep 3
+            echo "Application Deployed"
+         }
+      }
+   }
+}

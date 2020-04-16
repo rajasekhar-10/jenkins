c35a9eba2ec9414ec845cc646f3b32709a644c3d
Ravi <ravi2krishna@gmail.com>
2020-04-17 01:35:03 +0530

    credentials - aws - ack-sck

---
+++ 09-Jenkinsfile.groovy
@@ -0,0 +1,72 @@
+pipeline {
+   agent any
+   
+   environment {
+     // Global Var, but can be overidden by Local var defined in Stage
+     color = "Blank"
+     // These credentials would have been configured in Jenkins 
+     // with their respective credential IDs AWS-ACK and AWS-SCK
+     AWS_ACCESS_KEY_ID = credentials('AWS-ACK')
+     AWS_SECRET_ACCESS_KEY = credentials('AWS-SCK')
+   }
+   
+   stages {
+      stage('Fetch Code') {
+         steps {
+            git branch: 'master', url: 'https://github.com/ravi2krishna/JavaCalculator-Jenkins.git'
+            // environment variable used
+            echo "The Color is ${color}"
+         }
+      }
+      stage('Unit Testing') {
+         steps {
+            // using `true` to allow the Pipeline to continue nonetheless
+            sh 'mvn test || true'
+            junit '**/target/surefire-reports/*.xml'
+         }
+      }
+      stage('QA') {
+        when {
+            expression {
+            currentBuild.currentResult == 'UNSTABLE'
+            }
+        }
+         steps {
+            sleep 3
+            // Using Environmnet variable syntax - ${env.VARIABLE}
+            echo "Application NOT Deployed - Build ${env.BUILD_ID} is Unstable"
+            echo "Please go to ${BUILD_URL} and verify the build"
+         }
+      }
+      stage('AWS Fetch') {
+         steps {
+            // AWS IAM Credentials
+            withCredentials([string(credentialsId: 'AWS-ACK', variable: 'AWS-ACK-VAR'), string(credentialsId: 'AWS-SCK', variable: 'AWS-SCK-VAR')]) {
+                echo "Listing contents of an S3 bucket."
+                    sh "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+                        AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+                        AWS_REGION=us-east-1 \
+                        aws s3 ls"
+            }
+
+         }
+      }
+      stage('Deploy') {
+        environment {
+            // Local Var, scope is within Stage - Deploy 
+            color = "Green"  
+        }
+        when {
+            expression {
+            currentBuild.currentResult == 'SUCCESS'
+            }
+        }
+         steps {
+            sleep 3
+            echo "Application Deployed - Build ${env.BUILD_ID} is Stable"
+            echo "Please go to ${BUILD_URL} and verify the build"
+            echo "The Color is ${color}"
+         }
+      }
+   }
+}
